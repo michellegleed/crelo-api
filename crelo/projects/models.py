81e4161b0f88e0446e0dfc478feab710638de3de
Michelle Gleed <gleed.michelle@gmail.com>
2020-08-29 13:04:11 +0800

    about to attempt adding activity model

--- crelo/projects/models.py
+++ crelo/projects/models.py
@@ -14,7 +14,8 @@ class Location(models.Model):
     # slug_name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+        return self.name        
+
 
 class Pledgetype(models.Model):
     type = models.CharField(max_length=50)
