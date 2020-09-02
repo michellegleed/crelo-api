4a1c081ed766fdbeaa982803856f9c70b9d9eb5e
Michelle Gleed <gleed.michelle@gmail.com>
2020-09-02 19:35:51 +0800

    Updated settings for Heroku

--- crelo/crelo/settings.py
+++ crelo/crelo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,23 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oxp8=tipz89-a12f++7hfak7j#$y245_w^_d&-3%l*zw=s5w6+'
+# SECRET_KEY = 'oxp8=tipz89-a12f++7hfak7j#$y245_w^_d&-3%l*zw=s5w6+'
+
+# This checks for the environment variable called DJANGO_SECRET_KEY from Heroku, otherwise use this one.
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET_KEY',
+    'oxp8=tipz89-a12f++7hfak7j#$y245_w^_d&-3%l*zw=s5w6+'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+
+# Here, we want the DEBUG to be True when we are developing locally, but False in production. So we are checking for an environment variable (heruku will have one but localhost won't).
+# False needs to be set as a string! Because environment variables are stored as strings.
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -57,6 +69,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,3 +147,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
