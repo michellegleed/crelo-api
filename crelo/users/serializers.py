f45459c607dead21548e825be8e32f0441a50340
Michelle Gleed <gleed.michelle@gmail.com>
2020-09-02 20:34:38 +0800

    Removed extra fields on user model to create heroku superuser.

--- crelo/users/serializers.py
+++ crelo/users/serializers.py
@@ -6,23 +6,23 @@ from projects.models import ProjectCategory
 class CustomUserSerializer(serializers.Serializer):
 
     id = serializers.ReadOnlyField()
-    is_admin = serializers.ReadOnlyField()
+    # is_admin = serializers.ReadOnlyField()
     username = serializers.CharField(max_length=200)
     email = serializers.CharField(max_length=200)
     password = serializers.CharField(write_only=True)
-    location_id = serializers.IntegerField()
-    bio = serializers.CharField(max_length=2000, required=False)
-    image = serializers.URLField(required=False)
-    favourite_categories = serializers.PrimaryKeyRelatedField(queryset=ProjectCategory.objects.all(), many=True, required=False)
+    # location_id = serializers.IntegerField()
+    # bio = serializers.CharField(max_length=2000, required=False)
+    # image = serializers.URLField(required=False)
+    # favourite_categories = serializers.PrimaryKeyRelatedField(queryset=ProjectCategory.objects.all(), many=True, required=False)
 
     def create(self, validated_data):
 
         new_user = CustomUser.objects.create(
             username=validated_data['username'],
             email=validated_data['email'],
-            location_id=validated_data['location_id'],
-            bio = "",
-            image = ""
+            # location_id=validated_data['location_id'],
+            # bio = "",
+            # image = ""
         )
         new_user.set_password(validated_data['password'])
 
@@ -32,13 +32,13 @@ class CustomUserSerializer(serializers.Serializer):
 
     def update(self, instance, validated_data):
         instance.username = validated_data.get('username', instance.username)
-        instance.location_id = validated_data.get('location_id', instance.location_id)
-        instance.bio = validated_data.get('bio', instance.bio),
-        instance.image = validated_data.get('image', instance.image),
+        # instance.location_id = validated_data.get('location_id', instance.location_id)
+        # instance.bio = validated_data.get('bio', instance.bio),
+        # instance.image = validated_data.get('image', instance.image),
 
-        if 'favourite_categories' in validated_data:
-            cats = validated_data['favourite_categories']
-            instance.favourite_categories.set(cats)
+        # if 'favourite_categories' in validated_data:
+            # cats = validated_data['favourite_categories']
+            # instance.favourite_categories.set(cats)
 
         instance.save()
 
