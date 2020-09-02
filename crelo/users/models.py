d01c1b8d54f6b38dc8fa2f705e0cd834f1d97642
Michelle Gleed <gleed.michelle@gmail.com>
2020-09-02 20:42:13 +0800

    Actually removed extra fields (not just commented out)  on user model to create heroku superuser.

--- crelo/users/models.py
+++ crelo/users/models.py
@@ -4,15 +4,6 @@ from django.contrib.auth.models import AbstractUser
 from projects.models import ProjectCategory, Location
 
 class CustomUser(AbstractUser):
-    # location = models.ForeignKey(
-    #     Location, 
-    #     on_delete=models.CASCADE, 
-    #     related_name='user_location'
-    # )
-    # bio = models.TextField(blank=True, default="")
-    # image = models.URLField(blank=True, default="")
-    # is_admin = models.BooleanField(blank=True, default=False)
-    # favourite_categories = models.ManyToManyField(ProjectCategory, related_name='customuser', default=None, blank=True, null=True)
 
     def __str__(self):
         return self.username
