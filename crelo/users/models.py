8525f153ddb65f218f07075c5810424763cb111b
Michelle Gleed <gleed.michelle@gmail.com>
2020-09-14 16:11:47 +0800

    Removed location from user model so I can make a superuser

--- crelo/users/models.py
+++ crelo/users/models.py
@@ -4,15 +4,15 @@ from django.contrib.auth.models import AbstractUser
 from projects.models import ProjectCategory, Location
 
 class CustomUser(AbstractUser):
-    location = models.ForeignKey(
-        Location, 
-        on_delete=models.CASCADE, 
-        related_name='user_location'
-    )
+    # location = models.ForeignKey(
+    #     Location, 
+    #     on_delete=models.CASCADE, 
+    #     related_name='user_location'
+    # )
     bio = models.TextField(blank=True, default="")
     image = models.URLField(blank=True, default="")
     is_admin = models.BooleanField(blank=True, default=False)
-    favourite_categories = models.ManyToManyField(ProjectCategory, related_name='customuser', blank=True)
+    # favourite_categories = models.ManyToManyField(ProjectCategory, related_name='customuser', blank=True)
 
     def __str__(self):
         return self.username
