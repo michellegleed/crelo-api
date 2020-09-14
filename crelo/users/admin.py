30497bf6cab242ec4cbbc3f6de7406c9d1f4a0ea
Michelle Gleed <gleed.michelle@gmail.com>
2020-09-14 17:56:47 +0800

    Registered user model with admin

--- crelo/users/admin.py
+++ crelo/users/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
 # Register your models here.
+
+class MembershipInline(admin.TabularInline):
+    model = CustomUser.favourite_topics.through
+    
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', 'is_admin', 'location']
+    inlines = [
+        MembershipInline,
+    ]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
